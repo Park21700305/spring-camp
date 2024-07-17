64c1d4d8abe5383fe26402de5b54adfd7cbb77c3
ParkJiSung <gggg0195@gmail.com>
2024-07-17 10:04:38 +0900

    create:back folder

---
+++ back/example/src/test/java/spring/walab/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.walab.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
